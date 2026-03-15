19996983d10cd6a6cfb81397b9888415041798bc
hakim arhazzal <hakim.arhazzal@kenzup.com>
2026-03-15 02:47:34 +0000

    Fix plan-time subnet count

--- infrastructure/modules/network/local.tf
+++ infrastructure/modules/network/local.tf
@@ -1,5 +1,8 @@
 locals {
-  azs = slice(data.aws_availability_zones.available.names, 0, 2)
+  azs = [
+    "${var.aws_region}a",
+    "${var.aws_region}b",
+  ]
 
   vpc_cidr_block = "10.${var.aws_region == "us-east-1" ? 10 : 20}.0.0/16"
 }
